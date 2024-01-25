ea43d53c32ce163c8500036887b3ab00fdb6cd35
Job Monari <jobmonari7@gmail.com>
2024-01-25 03:59:57 +0300

    comments in elixir

--- first_app/lib/drop.ex
+++ first_app/lib/drop.ex
@@ -1,11 +1,15 @@
 defmodule Drop do
+  @doc """
+  these are called atoms, like pattern matching
+  """
   def multiplyings(:moon, num) when num >= 0 do
     num * num * 1.2
   end
-  @doc"""
+
+  @doc """
   these are called tuples, like pattern matching
   """
-  def multiplyings(:earth, num) when num >= 0 do
+  def multiplyings({:earth, num}) when num >= 0 do
     num * num * 1.3
   end
 end

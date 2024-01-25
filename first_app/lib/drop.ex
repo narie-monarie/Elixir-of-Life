a59e90289711dfd89e52b580bc0c56e7543848c5
Job Monari <jobmonari7@gmail.com>
2024-01-25 03:56:41 +0300

    comments

--- first_app/lib/drop.ex
+++ first_app/lib/drop.ex
@@ -2,7 +2,9 @@ defmodule Drop do
   def multiplyings(:moon, num) when num >= 0 do
     num * num * 1.2
   end
-
+  @doc"""
+  these are called tuples, like pattern matching
+  """
   def multiplyings(:earth, num) when num >= 0 do
     num * num * 1.3
   end

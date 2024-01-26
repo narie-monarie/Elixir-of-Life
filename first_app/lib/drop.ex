fe88225afc6c3a7a767825e056c5e20bd3112747
Job Monari <jobmonari7@gmail.com>
2024-01-26 15:20:44 +0300

    control structures

--- first_app/lib/drop.ex
+++ first_app/lib/drop.ex
@@ -13,7 +13,29 @@ defmodule Drop do
     num * num * 1.3
   end
 
-  def cattings() do
-    :result
+  def fall_velocity(planemo, distance) when distance >= 0 do
+    gravity =
+      case planemo do
+        :earth when distance >= 0 -> 9.8
+        :moon when distance >= 0 -> 1.6
+        :mars when distance >= 0 -> 3.71
+      end
+
+    velocity = :math.sqrt(2 * gravity * distance)
+
+    if velocity > 20 do
+      IO.puts("look below")
+    else
+      IO.puts("Reasonable...")
+    end
+  end
+
+  def countdown(from) when from > 0 do
+    IO.inspect(from)
+    countdown(from - 1)
+  end
+
+  def countdown(_from) do
+    IO.puts("blastoff!")
   end
 end
